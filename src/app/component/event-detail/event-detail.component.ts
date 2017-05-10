035a0df3e21311d6da073c9eaca46730ec429419
Manfred Harrer <manfred.harrer@jambit.com>
2017-05-10 09:33:40 +0200

    added participants view to event detail component

--- src/app/component/event-detail/event-detail.component.ts
+++ src/app/component/event-detail/event-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {LoginService} from "../../service/login.service";
 import {Location} from "@angular/common";
@@ -6,41 +6,50 @@ import {CommonComponent} from "../common.component";
 import {EventService} from "../../service/event.service";
 import {Event} from "../../model/event";
 import {DataService} from "../../service/data.service";
-import {EventType} from "../../enum/EventType";
 
 @Component({
-  selector: 'app-event-detail',
-  templateUrl: './event-detail.component.html',
-  styleUrls: ['./event-detail.component.scss']
+    selector: 'app-event-detail',
+    templateUrl: './event-detail.component.html',
+    styleUrls: ['./event-detail.component.scss']
 })
-export class EventDetailComponent extends CommonComponent  implements OnInit {
+export class EventDetailComponent extends CommonComponent implements OnInit {
 
-  event: Event;
+    selectedEvent:Event;
 
-  constructor(loginService: LoginService, route: ActivatedRoute, private eventService: EventService,
-              location:Location, private dataService: DataService) {
-    super(loginService, location);
-  }
+    event:Event;
 
-  ngOnInit() {
-    this.event = this.dataService.currentEvent;
-  }
-
-  save():void {
-    this.eventService.update(this.event)
-        .then(() => this.goBack());
-  }
-
-  getEventImage(eventType) {
-    switch(eventType) {
-      case 'match':
-        return "../../assets/Spiel_500.jpg";
-      case 'tournament':
-        return "../../assets/Turnier_500.jpg";
-      case 'teamevent':
-        return "../../assets/Teamevent_500.jpg";
-      default: // training
-        return "../../assets/Training_500.jpg";
+    constructor(loginService:LoginService, route:ActivatedRoute, private eventService:EventService,
+                location:Location, private dataService:DataService) {
+        super(loginService, location);
     }
-  }
+
+    selectEvent() {
+        this.selectedEvent = this.event;
+    }
+
+    unselectEvent() {
+        this.selectedEvent = null;
+    }
+
+    ngOnInit() {
+        this.event = this.dataService.currentEvent;
+    }
+
+    save():void {
+        this.eventService.update(this.event)
+            .then(() => this.goBack());
+    }
+
+    getEventImage(eventType) {
+        switch (eventType) {
+            case 'match':
+                return "../../assets/Spiel_500.jpg";
+            case 'tournament':
+                return "../../assets/Turnier_500.jpg";
+            case 'teamevent':
+                return "../../assets/Teamevent_500.jpg";
+            default: // training
+                return "../../assets/Training_500.jpg";
+        }
+    }
 }
