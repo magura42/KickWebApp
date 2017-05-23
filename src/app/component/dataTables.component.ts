c88aa2005ab98f1d9975e2a8f2229c5004a238d0
Manfred Harrer <manfred.harrer@jambit.com>
2017-05-23 15:20:18 +0200

    maintenance

--- src/app/component/dataTables.component.ts
+++ src/app/component/dataTables.component.ts
@@ -1,14 +1,12 @@
-
 import {LoginService} from "../service/login.service";
 import {Location} from "@angular/common";
 import {CommonComponent} from "./common.component";
-import Settings = DataTables.Settings;
 
 export class DataTablesComponent extends CommonComponent {
 
-    dtOptions: Settings = {};
+    dtOptions:DataTables.Settings = {};
 
-    constructor(loginService: LoginService, location: Location) {
+    constructor(loginService:LoginService, location:Location) {
         super(loginService, location)
         this.setDataTablesOption();
     }
@@ -17,27 +15,26 @@ export class DataTablesComponent extends CommonComponent {
         this.dtOptions = {
             paging: true,
             searching: false,
-            order: [[ 0, "desc" ]],
-            language:
-            {
-                "emptyTable":      "Keine Daten in der Tabelle vorhanden",
-                "info":            "_START_ bis _END_ von _TOTAL_ Einträgen",
-                "infoEmpty":       "0 bis 0 von 0 Einträgen",
-                "infoFiltered":    "(gefiltert von _MAX_ Einträgen)",
-                "infoPostFix":     "",
-                "lengthMenu":      "_MENU_ Einträge anzeigen",
-                "loadingRecords":  "Wird geladen...",
-                "processing":      "Bitte warten...",
-                "search":          "Suchen",
-                "zeroRecords":     "Keine Einträge vorhanden.",
+            order: [[0, "desc"]],
+            language: {
+                "emptyTable": "Keine Daten in der Tabelle vorhanden",
+                "info": "_START_ bis _END_ von _TOTAL_ Einträgen",
+                "infoEmpty": "0 bis 0 von 0 Einträgen",
+                "infoFiltered": "(gefiltert von _MAX_ Einträgen)",
+                "infoPostFix": "",
+                "lengthMenu": "_MENU_ Einträge anzeigen",
+                "loadingRecords": "Wird geladen...",
+                "processing": "Bitte warten...",
+                "search": "Suchen",
+                "zeroRecords": "Keine Einträge vorhanden.",
                 "paginate": {
-                    "first":       "Erste",
-                    "previous":    "Zurück",
-                    "next":        "Nächste",
-                    "last":        "Letzte"
+                    "first": "Erste",
+                    "previous": "Zurück",
+                    "next": "Nächste",
+                    "last": "Letzte"
                 },
                 "aria": {
-                    "sortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+                    "sortAscending": ": aktivieren, um Spalte aufsteigend zu sortieren",
                     "sortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
                 }
             }
